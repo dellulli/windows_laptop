d000147e40585628d681d86189c372ff016184c3
dellulli <177489710+bbokari121@users.noreply.github.com>
2026-01-23 22:25:00 +0530

    webcam v9

--- src/App.jsx
+++ src/App.jsx
@@ -152,8 +152,7 @@ const [downloadsPos, setDownloadsPos] = useState({ x: 50, y: 550 })
         const faceLandmarker = await FaceLandmarker.createFromOptions(vision, {
           baseOptions: {
             modelAssetPath:
-'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.8/wasm/face_landmarker.task',
-
+              'https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task',
           },
           runningMode: 'VIDEO',
           numFaces: 1,
